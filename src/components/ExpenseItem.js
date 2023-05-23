f3aa708e9a27e402ccca1d1285e316e49df8c483
Hiago Koziel <hiagokoziel100@gmail.com>
2023-05-23 12:58:56 +0200

    Adding component ExpenseDate and passing values between two components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem(props) { 
+function ExpenseItem(props) {
+
 
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate date={props.date}/>
             <div className='expenseitem__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount}</div>
