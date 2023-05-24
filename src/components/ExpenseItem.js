11bc41e0363e1fe48d5dac5ff8029e5405e3af95
Hiago Koziel <hiagokoziel100@gmail.com>
2023-05-24 14:16:07 +0200

    Passing data through components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,10 +3,9 @@ import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
 
-
     return (
         <div className='expense-item'>
-            <ExpenseDate date={props.date}/>
+            <ExpenseDate date={props.date} />
             <div className='expenseitem__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount}</div>
